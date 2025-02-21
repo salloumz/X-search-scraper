164515a027b9e91239be79062ace7b8eaea9cf23
salloumz <yousefsal37@gmail.com>
2025-02-21 02:34:45 -0500

    rework

--- scraper.py
+++ scraper.py
@@ -1,21 +1,34 @@
-import requests
-from bs4 import BeautifulSoup
+import asyncio
+from twscrape import API, gather
+from twscrape.logger import set_log_level
+import pandas as pd
 
-def scrape():
-    url = 'https://www.example.com'
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    
-    title = soup.select_one('h1').text
-    text = soup.select_one('p').text
-    link = soup.select_one('a').get('href')
-    
-    print(title)
-    print(text)
-    print(link)
+L = 500
 
+async def main():
 
+    # User Inputs
+    keywords = input("Enter the keywords you want to search for: ")
+    since_date = input("Enter your query start date (YYYY-MM-DD): ")
+    until_date = input("Enter your query end date (YYYY-MM-DD): ")
 
-if __name__ == '__main__':
-    scrape()
+    query_parts = [keywords.strip()]
+    if since_date.strip():
+            query_parts.append(f"since:{since_date.strip()}")
+    if until_date.strip():
+            query_parts.append(f"until:{until_date.strip()}")        
 
+    query = " ".join(query_parts)
+
+    api = API()
+    tweets = []
+    q = query
+    async for tweet in api.search(q, limit = L):
+        tweets.append([tweet.date, tweet.user.username, tweet.rawContent])
+        
+    df = pd.DataFrame(tweets, columns=['Date', 'User', 'Tweet'])
+    df.to_csv('scraped_tweets.csv', index=False, encoding='utf-8')
+    df.to_json('scraped_tweets.json', orient='records', lines=True)
+
+if __name__ == "__main__":
+    asyncio.run(main())
